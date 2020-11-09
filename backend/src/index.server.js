a94242ea5bf3897af0f7887cf4bbcd6e07e37079
Ravi Kiran <ravi.kiran8305@gmail.com>
2020-11-09 07:20:15 +0530

    update

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const env = require('dotenv')
+const { response } = require('express')
 const app = express()
-
-
 //environment variable
 env.config()
-
+// get
+app.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Hello , you are talking to the server'
+    })
+})
+// post
+app.post('/data',(req,res,next)=>{
+    res.status(200).json({
+        message: req.body
+    })
+})
+// app listen
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is Running on Port ${process.env.PORT}`)
 })
